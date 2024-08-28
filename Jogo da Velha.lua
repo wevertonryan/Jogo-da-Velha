b66cf724a35b53a585afec54be3a941c85adae63
Weverton Ryan <wevertonryan205@gmail.com>
2024-08-27 21:10:19 -0300

    vitoria

--- Jogo da Velha.lua
+++ Jogo da Velha.lua
@@ -14,6 +14,9 @@ function vald() --valida a resposta que o jogador deu de uma forma que o codigo 
     if n == "" then
       n = " "
     end
+    if l == "" then
+      l = " "
+    end
     if string.byte(l) >= 65 and string.byte(l) <= list["afl"] and string.byte(n) >= 48 and string.byte(n) <= list["nml"] then
       --65 é A em byte, 90 é Z em byte, assim ele verifica se é realmente uma letra
       --é a mesma coisa com o número
@@ -32,18 +35,11 @@ function vald() --valida a resposta que o jogador deu de uma forma que o codigo 
   return l .. n --retorna valor padronizado
 end
 
-function map(x) --mapeia o jogo em tela como jogo da velha
-  if x == 1 then --apresenta o guia de linha(letra) e coluna(número)
-    print("  1 2 3")
-    for i = 1, 3 do
-      local txt = string.char(64 + i)
-      print(txt.." "..list[txt..1].."|"..list[txt..2].."|"..list[txt..3])
-    end
-  else --para o jogo real
-    for i = 1, 3 do
-      local txt = string.char(64 + i)
-      print(list[txt..1].."|"..list[txt..2].."|"..list[txt..3])
-    end
+function map() --mapeia o jogo em tela como jogo da velha
+  print("  1 2 3")
+  for i = 1, 3 do
+    local txt = string.char(64 + i)
+    print(txt.." "..list[txt..1].."|"..list[txt..2].."|"..list[txt..3])
   end
 end
 
@@ -56,6 +52,7 @@ function rule()
       val = false
     else
       list[resp] = p
+      endgame = verf(resp)
       val = true
     end
   until val == true
@@ -66,12 +63,48 @@ function rule()
   end
 end
 
+function verf(m)
+  local le = string.byte(string.sub(m, 1, 1))
+  local nu = string.byte(string.sub(m, 2, 2))
+  local x = {-1, 0, 1, 1}
+  local y = {-1, -1, -1, 0}
+  for i = 1, 4 do
+    --print(string.char(le+x[i]*-1)..string.char(nu+y[i]*-1))
+    --print(string.char(le+x[i])..string.char(nu+y[i]))
+    if list[string.char(le+x[i])..string.char(nu+y[i])] == p then
+      print("check1")
+      if list[string.char(le+x[i]*2)..string.char(nu+y[i]*2)] == p then
+        return p
+      elseif list[string.char(le+x[i]*-1)..string.char(nu+y[i]*-1)] == p then
+        return p
+      end
+    elseif list[string.char(le+x[i]*-1)..string.char(nu+y[i]*-1)] == p then
+      print("check2")
+      if list[string.char(le+x[i]*-2)..string.char(nu+y[i]*-2)] == p then
+        print("check3")
+        return p
+      end
+    end
+  end  
+end
+
+
 print("[[JOGO DA VELHA]] versão teste de mapeamento\npara jogar apenas digite de A a C mais um número de 1 a 3, exemplo: C3")
-map(1)
+map()
 p = "X"
-while 1 == 1 do
+endgame = ""
+repeat
+  print(endgame == p)
   rule()
-  map(2)
+  os.execute("clear")
+  map()
+until endgame == "X" or endgame == "O"
+if endgame == "X" then
+  print("O vencedor é o jogador 1")
+elseif endgame == "O" then
+  print("O vencedor é o jogador 2")
+else
+  print("empate")
 end
 
 --(feito) impossibilitar mandar respostas que não estejam em letra número
